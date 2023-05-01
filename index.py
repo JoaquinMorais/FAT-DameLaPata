825e747533d35a0016b0834d619e2f5bf7c41ce9
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-05-01 16:55:55 -0300

    mejorando

--- index.py
+++ index.py
@@ -3,13 +3,17 @@ from utils.db import db
 from models.users import *
 
 from routes.home import Home
-from routes.login import Login
+from routes.login_google import Login_Google
+
+app.config.update(
+SECRET_KEY='MySecretKey'
+)
 
 with app.app_context():
     db.create_all()
 
-app.register_blueprint(Home)
-app.register_blueprint(Login)
+#app.register_blueprint(Home)
+app.register_blueprint(Login_Google)
 
 if __name__ == '__main__':
     app.run(debug=True)
