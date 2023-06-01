618115026b7ef1843ba4361a48fffbd8031132bd
Marco <marco.fini22@gmail.com>
2023-06-01 11:15:10 -0300

    salo labura

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Header() {
+  return <h1>Header</h1>;
+}
+
+function Sidebar() {
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  );
+}
+
+function Content() {
+  return <p>Contenido principal</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>ALEJO SE LA COME</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div>
+        <Sidebar />
+        <Content />
+      </div>
     </div>
   );
 }
