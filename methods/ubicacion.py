8773cea7c798d3a95bb6121599b5650999183d75
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-06-06 10:56:42 -0300

    Ahora puede tomar varios lugares a la vez

--- methods/ubicacion.py
+++ methods/ubicacion.py
@@ -4,7 +4,18 @@ import math
 
 geo = Nominatim(user_agent = "MyApp")
 
-place = input("Dime un lugar: ")
-loc = geo.geocode(place)
-print(loc)
-print("Lat:", loc.latitude, "Lon:", loc.longitude)
+# Un lugar a la vez
+def OnePlace():
+    place = input("Dime un lugar: ")
+    loc = geo.geocode(place)
+    print(loc)
+    print("Lat:", loc.latitude, "Lon:", loc.longitude)
+
+# Varios lugares a la vez
+def ManyPlaces():
+    place_list = ["Sevilla", "Madrid", "Cordoba"]
+    for i in place_list:
+        address = geo.geocode(i, timeout=3)
+        print('Ciudad: ' + i + " " + str(address.latitude) + " " + str(address.longitude))
+        time.sleep(1)
+
