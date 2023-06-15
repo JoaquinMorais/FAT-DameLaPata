d886883e9f7cb08a93a9a87fb4b473e937306ecd
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-06-15 11:24:25 -0300

    Calculos aprox

--- methods/social_status.py
+++ methods/social_status.py
@@ -1,16 +1,14 @@
 # La idea es calcular el estatus con varias preguntas y segun la respuesta sume determinados puntos para despues
 # sacar una conclusion...
-rate = 0.0
+rate = 0
 
 # Calcular estatus Sueldo
 ingreso = int(input("Ingresos aproximado: "))
 if ingreso >= 150000:
-    rate += 1.5
+    rate += 10
 elif ingreso <= 75000:
-    rate += 0.5
+    rate += 5
 else:
     rate += 1
-
-print(rate)
 
 # Calcular estatus Casa
