47a34635c93fc356726a412bed6a4cb888240272
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-06-15 11:17:53 -0300

    Calculos aprox

--- methods/social_status.py
+++ methods/social_status.py
@@ -1,3 +1,14 @@
+
+rate = 0.0
+
 # Calcular estatus Sueldo
+ingreso = int(input("Ingresos aproximado: "))
+if ingreso >= 150000:
+    rate += 1.5
+elif ingreso <= 75000:
+    rate += 0.5
+else:
+    rate += 1
+print(rate)
 
 # Calcular estatus Casa
