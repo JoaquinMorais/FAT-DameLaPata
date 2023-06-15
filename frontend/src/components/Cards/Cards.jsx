7c912303a9b6b410c3413dad2763fc92aea75275
Agus Salonia <agusalonia04@gmail.com>
2023-06-15 12:00:58 -0300

    cambios

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-export const Cards = () => {
+export const Cards = (props) => {
   return (
-    <>
+    <div className='carta-container'>
+      <a className='carta-click' href="">
         <div className="carta">
-            <img src="" alt="imagen" />
-            <h3>Nombre</h3>
-            <p>Edad - Tamaño</p>
+          <img src={require(`../../images/Dogs/perros-${props.nombre}.jpg`)} alt="imagen" />
         </div>
-    </>
+        <div className="texto-carta">
+          <h3>{ props.nombre }</h3>
+          <h6>{ props.edad } - { props.tamaño }</h6>
+          <p className='click'>Click para mas información</p>
+        </div>
+      </a>
+    </div>
   )
 }
--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,14 +1,72 @@
 import React from 'react';
-import { Header } from '../components/Header/Header'
-import { Cards } from '../components/Cards/Cards'
-import { Navbar } from '../components/Navbar/Navbar'
+import { Header } from '../components/Header/Header';
+import { Cards } from '../components/Cards/Cards';
+import { Navbar } from '../components/Navbar/Navbar';
+import { Footer } from '../components/Footer/Footer';
+import '.././index.css';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
-      <Cards />
+      <div className="dogs">
+        <Cards 
+        nombre = 'Max'
+        edad = '6'
+        tamaño = 'Grande'
+        />
+
+        <Cards 
+        nombre = 'Ryan'
+        edad = '3'
+        tamaño = 'Grande'
+        />
+
+        <Cards 
+        nombre = 'Wachin'
+        edad = '11'
+        tamaño = 'Pequeño'
+        />
+
+        <Cards 
+        nombre = 'Tobi'
+        edad = '8'
+        tamaño = 'Pequeño'
+        />
+
+        <Cards 
+        nombre = 'Rufian'
+        edad = '2'
+        tamaño = 'Mediano'
+        />
+
+        <Cards 
+        nombre = 'SonGoku'
+        edad = '5'
+        tamaño = 'Grande'
+        />
+
+        <Cards 
+        nombre = 'Hit'
+        edad = '1'
+        tamaño = 'Pequeño'
+        />
+
+        <Cards 
+        nombre = 'Ler'
+        edad = '1'
+        tamaño = 'Pequeño'
+        />
+
+        <Cards 
+        nombre = 'Juan'
+        edad = '10'
+        tamaño = 'Mediano'
+        />
+
+        
+      </div>
     </div>
   );
 }
