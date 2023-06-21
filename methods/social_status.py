f1dbdfe387c87b27855f252879c669260886800f
Yaco <babiachukyaco@gmail.com>
2023-06-21 11:19:57 -0300

    Actualizacion

--- methods/social_status.py
+++ methods/social_status.py
@@ -19,13 +19,28 @@ def stone(pet, sum1, sum2, points):
         points += sum2
     return points
 
-pets = str(input("Tiene mas mascotas(y/n): "))
+pets = str(input("Actualmente tiene mas de una mascota(y/n): "))
 points = stone(pets, 10, 5, points)
 
 pets = str(input("Has tenido alguna mascota(y/n): "))
-points = stone(pets, 15, 5, points)
+points = stone(pets, 8, 5, points)
 
 # Calcular estatus Casa
+
+pets = str(input("¿Tiene patio o espacio abierto?(y/n): "))
+points = stone(pets, 10, 3, points)
+
+pets = str(input("¿Estas Casado?(y/n): "))
+points = stone(pets, 8, 4, points)
+
+pets = str(input("¿Tienes un título universitario?(y/n): "))
+points = stone(pets, 6, 4, points)
+
+pets = str(input("¿Tienes acceso a servicios de salud privados?(y/n): "))
+points = stone(pets, 6, 2, points)
+
+pets = str(input("¿Tu ocupación actual te permite vivir cómodamente?(y/n): "))
+points = stone(pets, 10, 4, points)
 
 
 print(points)
