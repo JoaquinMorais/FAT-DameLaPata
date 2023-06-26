5c8abf32a5c82836ae1a15ff7c11396962bdc6f7
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-06-26 18:16:35 -0300

    Calculo final terminado

--- methods/social_status.py
+++ methods/social_status.py
@@ -1,17 +1,18 @@
 # La idea es calcular el estatus con varias preguntas y segun la respuesta sume determinados puntos para despues
 # sacar una conclusion...
 points = 0
+result = 0
 
 # Calcular estatus Sueldo
 income = int(input("Ingresos aproximado(Ej: 150000): "))
 if income >= 150000:
     points += 10
 elif income <= 75000:
-    points += 1
+    points += 2
 else:
     points += 5
 
-# Tiene mas mascotas???
+# Funcion default de calculo
 def stone(pet, sum1, sum2, points):
     if pet == "y":
         points += sum1
@@ -19,28 +20,42 @@ def stone(pet, sum1, sum2, points):
         points += sum2
     return points
 
-pets = str(input("Actualmente tiene mas de una mascota(y/n): "))
+# Tiene mas mascotas???
+pets = str(input("¿Actualmente tiene mas de una mascota?(y/n): "))
 points = stone(pets, 10, 5, points)
 
-pets = str(input("Has tenido alguna mascota(y/n): "))
+pets = str(input("¿Has tenido mascotas antes?(y/n): "))
 points = stone(pets, 8, 5, points)
 
 # Calcular estatus Casa
-
-pets = str(input("¿Tiene patio o espacio abierto?(y/n): "))
-points = stone(pets, 10, 3, points)
+pets = str(input("¿Tienes el espacio adecuado en tu hogar para albergar a la mascota de forma segura y cómoda?(y/n): "))
+points = stone(pets, 8, 3, points)
 
 pets = str(input("¿Estas Casado?(y/n): "))
-points = stone(pets, 8, 4, points)
-
-pets = str(input("¿Tienes un título universitario?(y/n): "))
 points = stone(pets, 6, 4, points)
 
-pets = str(input("¿Tienes acceso a servicios de salud privados?(y/n): "))
+pets = str(input("¿Tienes el secundario completado?(y/n): "))
 points = stone(pets, 6, 2, points)
 
+pets = str(input("¿Tienes acceso a servicios de salud privados?(y/n): "))
+points = stone(pets, 8, 4, points)
+
 pets = str(input("¿Tu ocupación actual te permite vivir cómodamente?(y/n): "))
-points = stone(pets, 10, 4, points)
+points = stone(pets, 9, 3, points)
+
+pets = str(input("¿Tienes los recursos necesarios para mantener todas las necesidades de la mascota?(y/n): "))
+points = stone(pets, 10, 2, points)
 
 
-print(points)
+# Calculos finales (max= 75;min= 30) (1=Bueno, 2=Mediocre, 3=Malo)
+
+def final_calculation():
+    if points <= 60:
+        result = 1
+    elif points <= 45:
+        result = 3
+    else:
+        result = 2
+    print(result)
+
+final_calculation()
