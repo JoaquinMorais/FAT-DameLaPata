96f20cd9ca94d43dd977af8861cfbea9b2229207
Juan Pablo Genaro <jpgenaro06@gmail.com>
2023-08-04 11:20:25 -0300

    Mas cosas :)

--- frontend/src/components/Dogs/Filters/Filters.jsx
+++ frontend/src/components/Dogs/Filters/Filters.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center; /* Centrar verticalmente */
+  margin-bottom: 20px;
+`;
+
+const Title = styled.h3`
+  font-size: 18px;
+  margin-right: 10px;
+`;
+
+const Button = styled.button`
+  padding: 8px 12px;
+  font-size: 14px;
+  background-color: #3498db;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  margin-right: 10px; /* Agregar espacio entre los botones */
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #2980b9;
+  }
+`;
 
 function Filters() {
-    return (
-        <content>
-            <h3>Filtrar por: </h3>
-            <button>Edad</button>
-        </content>
-    )
-};
+  return (
+    <Content>
+      <Title>Filtrar por:</Title>
+      <Button>Edad</Button>
+      <Button>Tamaño</Button>
+      <Button>Especie</Button>
+      <Button>Peso</Button>
+      <Button>Color</Button>
+    </Content>
+  );
+}
 
 export default Filters;
