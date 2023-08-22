b72e2f66d6761061506c240c33ce0ca2937e5d5e
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-08-22 09:32:36 -0300

    a

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://bdi:pepe1234@localhost/DameLaPata'
+app.config['SQLALCHEMY_DATABASE_URI']= '''mysql+pymysql://
+bdi:
+pepe1234@
+localhost/
+DameLaPata'''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
