b6344d97488405b2b6e414361c029b98db30eb23
JPGenaro <jpgenaro06@gmail.com>
2023-08-24 10:51:31 -0300

    desieñando los botones de filter

--- frontend/src/components/Dogs/Filters/Filters.jsx
+++ frontend/src/components/Dogs/Filters/Filters.jsx
@@ -30,16 +30,31 @@ const Button = styled.button`
   }
 `;
 
-function Filters({ onColorFilter }) {
-  const [showColorButtons, setShowColorButtons] = useState(false);
+const CheckboxGroup = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-  const handleColorButtonClick = () => {
-    setShowColorButtons(!showColorButtons);
+const CheckboxLabel = styled.label`
+  font-size: 14px;
+  margin-right: 10px;
+`;
+
+function Filters({ onColorFilter }) {
+  const [showColorCheckboxes, setShowColorCheckboxes] = useState(false);
+  const [selectedColors, setSelectedColors] = useState([]);
+
+  const handleColorCheckboxChange = (color) => {
+    if (selectedColors.includes(color)) {
+      setSelectedColors(selectedColors.filter((c) => c !== color));
+    } else {
+      setSelectedColors([...selectedColors, color]);
+    }
   };
 
-  const handleColorSelection = (color) => {
-    onColorFilter(color);
-    setShowColorButtons(false);
+  const applyColorFilters = () => {
+    onColorFilter(selectedColors);
+    setShowColorCheckboxes(false);
   };
 
   return (
@@ -50,14 +65,39 @@ function Filters({ onColorFilter }) {
       <Button>Especie</Button>
       <Button>Peso</Button>
       <div>
-        <Button onClick={handleColorButtonClick}>Color</Button>
-        {showColorButtons && (
-          <div>
-            <Button onClick={() => handleColorSelection('red')}>Rojo</Button>
-            <Button onClick={() => handleColorSelection('blue')}>Azul</Button>
-            <Button onClick={() => handleColorSelection('green')}>Verde</Button>
-            {/* Agregar más botones según los colores en tu data */}
-          </div>
+      <Button onClick={() => setShowColorCheckboxes(!showColorCheckboxes)}>Color</Button>
+        {showColorCheckboxes && (
+          <CheckboxGroup>
+            <CheckboxLabel>
+              <input
+                type="checkbox"
+                value="red"
+                checked={selectedColors.includes('red')}
+                onChange={() => handleColorCheckboxChange('red')}
+              />
+              Rojo
+            </CheckboxLabel>
+            <CheckboxLabel>
+              <input
+                type="checkbox"
+                value="blue"
+                checked={selectedColors.includes('blue')}
+                onChange={() => handleColorCheckboxChange('blue')}
+              />
+              Azul
+            </CheckboxLabel>
+            <CheckboxLabel>
+              <input
+                type="checkbox"
+                value="green"
+                checked={selectedColors.includes('green')}
+                onChange={() => handleColorCheckboxChange('green')}
+              />
+              Verde
+            </CheckboxLabel>
+            {/* Agregar más checkboxes según los colores en tu data */}
+            <Button onClick={applyColorFilters}>Aplicar</Button>
+          </CheckboxGroup>
         )}
       </div>
     </Content>
