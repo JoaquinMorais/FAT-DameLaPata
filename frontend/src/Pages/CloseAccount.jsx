4438793432241546212947ff89f1c9680f927ca0
JPGenaro <jpgenaro06@gmail.com>
2023-09-07 11:19:04 -0300

    mejoras de la ventana 2

--- frontend/src/Pages/CloseAccount.jsx
+++ frontend/src/Pages/CloseAccount.jsx
@@ -1,31 +1,50 @@
 import React, { useState } from 'react';
-import ConfirmDialog from '../components/CloseAccount/ConfirmDialog'
+import ConfirmDialog from '../components/CloseAccount/ConfirmDialog';
+import SuccessDialog from '../components/CloseAccount/SuccessDialog';
 
 const CloseAccount = () => {
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+    const [isAccountDeleted, setIsAccountDeleted] = useState(false);
 
-    const openDialog = () => {
-        setIsDialogOpen(true);
+    const openConfirmation = () => {
+        setIsConfirmationOpen(true);
     };
 
-    const closeDialog = () => {
-        setIsDialogOpen(false);
+    const closeConfirmation = () => {
+        setIsConfirmationOpen(false);
     };
 
     const handleDeleteAccount = () => {
-        // Agregar la lógica para borrar la cuenta aquí
-        // Puedes usar axios u otra librería para hacer la solicitud DELETE
-        closeDialog();
+        // Agregar la lógica para borrar la cuenta aquí.
+        // Puedes usar axios u otra librería para hacer la solicitud DELETE.
+        // Cuando la cuenta se haya eliminado con éxito, cambia el estado isAccountDeleted a true.
+        // Por ejemplo:
+        // Realizar la eliminación de la cuenta...
+        // Después de la eliminación, cambiar el estado isAccountDeleted a true.
+        setIsAccountDeleted(true);
+        closeConfirmation();
+    };
+
+    const closeSuccessDialog = () => {
+        setIsAccountDeleted(false);
     };
 
     return (
         <>
-            <button onClick={openDialog}>Delete Account</button>
-            <ConfirmDialog
-                isOpen={isDialogOpen}
-                onClose={closeDialog}
-                onConfirm={handleDeleteAccount}
-            />
+            <button onClick={openConfirmation}>Delete Account</button>
+            {isConfirmationOpen && !isAccountDeleted && (
+                <ConfirmDialog
+                    isOpen={isConfirmationOpen}
+                    onClose={closeConfirmation}
+                    onConfirm={handleDeleteAccount}
+                />
+            )}
+            {isAccountDeleted && (
+                <SuccessDialog
+                    isOpen={isAccountDeleted}
+                    onClose={closeSuccessDialog}
+                />
+            )}
         </>
     );
 };
