daa5cd4b695cc81e98220558421acaa17a52f8f3
JPGenaro <jpgenaro06@gmail.com>
2023-09-07 11:11:53 -0300

    listo ventana 1

--- frontend/src/components/CloseAccount/ConfirmDialog.jsx
+++ frontend/src/components/CloseAccount/ConfirmDialog.jsx
@@ -3,26 +3,48 @@ import Modal from 'react-modal';
 import { styled } from 'styled-components';
 import Button from '@mui/material/Button';
 
+const buttonStyle = {
+    marginRight: '10px', // Separación entre botones
+};
+
+const modalStyle = {
+    content: {
+        maxWidth: '400px', // Ajusta el ancho máximo
+        maxHeight: '200px', // Ajusta la altura máxima
+        margin: 'auto',
+        borderRadius: '8px',
+        padding: '20px',
+        textAlign: 'center',
+        border: '4px solid #000', // Borde más grueso
+    },
+};
+
 const ConfirmDialog = ({ isOpen, onClose, onConfirm }) => {
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onClose}
-            style={{
-                content: {
-                    maxWidth: '400px', // Ajusta el ancho máximo
-                    maxHeight: '200px', // Ajusta la altura máxima
-                    margin: 'auto',
-                    borderRadius: '8px',
-                    padding: '20px',
-                    textAlign: 'center',
-                },
-            }}
+            style={modalStyle}
         >
             <h2>Confirmación</h2>
             <p>¿Estás seguro de que deseas borrar tu cuenta?</p>
-            <Button onClick={onConfirm} variant="contained" color="success">Sí, borrar cuenta</Button>
-            <Button onClick={onClose} variant="outlined" color="error">Cancelar</Button>
+            <div style={{ marginTop: '20px' }}>
+                <Button
+                    onClick={onConfirm}
+                    variant="outlined"
+                    color="error"
+                    style={buttonStyle}
+                >
+                    Sí, borrar cuenta
+                </Button>
+                <Button
+                    onClick={onClose}
+                    variant="contained"
+                    color="success"
+                >
+                    Cancelar
+                </Button>
+            </div>
         </Modal>
     );
 };
