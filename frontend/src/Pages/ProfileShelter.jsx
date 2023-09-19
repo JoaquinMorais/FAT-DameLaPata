fd0449f866652f436f9841aa0b3e0f3946332b1d
JPGenaro <jpgenaro06@gmail.com>
2023-09-19 10:51:34 -0300

    detalles y correciones hechas

--- frontend/src/Pages/ProfileShelter.jsx
+++ frontend/src/Pages/ProfileShelter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
