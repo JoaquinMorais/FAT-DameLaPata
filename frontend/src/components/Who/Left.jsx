3968ac6a9d67975927b8f16a8b2a182666c442cf
JPGenaro <jpgenaro06@gmail.com>
2023-09-19 10:15:31 -0300

    corrigiendo

--- frontend/src/components/Who/Left.jsx
+++ frontend/src/components/Who/Left.jsx
@@ -1,56 +1,75 @@
-  import React from 'react';
-  import { styled } from 'styled-components';
-  import Fade from 'react-reveal/Fade';
+import React from 'react';
+import { styled } from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
-  const LeftContent = ({ text, imageUrl }) => {
-    return (
-      <Wrap>
-        <Fade bottom>
-          <Container>
-            <ImgPart src={imageUrl} />
-            <Txt>{text}</Txt>
-          </Container>
-        </Fade>
-      </Wrap>
-    );
-  };
+const LeftContent = ({ text, imageUrl }) => {
+  return (
+    <Wrap>
+      <Fade bottom>
+        <Container>
+          <Txt>{text}</Txt>
+          <ImgPart src={imageUrl} />
+        </Container>
+      </Fade>
+    </Wrap>
+  );
+};
 
-  export default LeftContent;
+export default LeftContent;
 
-  const Wrap = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: 1fr 1fr; /* Divide en dos columnas en pantallas grandes */
-    gap: 20px;
-    @media (max-width: 1024px) {
-      grid-template-columns: 1fr; /* Cambia a una sola columna en pantallas pequeñas */
-    }
-  `;
+const Wrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-  const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-  `;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%; /* Ajusta el ancho del contenedor según tus preferencias */
+  margin: 0 auto; /* Centra el contenido horizontalmente */
 
-  const Txt = styled.p`
-    font-size: 22px;
-    text-align: justify;
-    margin-top: 40px;
-    @media (max-width: 1024px) {
-      text-align: center;
-      margin-top: 30px;
-      font-size: 20px;
-    }
-  `;
+  @media (max-width: 1024px) {
+    flex-direction: column; /* Cambia a una columna en pantallas más pequeñas */
+    align-items: flex-start;
+  }
 
-  const ImgPart = styled.img`
-    object-fit: cover;
-    height: auto;
-    @media (max-width: 1024px) {
-      width: 100%;
-      height: 200px;
-    }
-    @media (max-width: 425px) {
-      min-height: 350px;
-    }
-  `;
+  @media (min-width: 1024px) {
+    padding: 0 20px; /* Reduce el espacio en los costados en pantallas más grandes */
+  }
+`;
+
+const Txt = styled.p`
+  font-size: 22px;
+  text-align: justify;
+  margin-top: 40px;
+  margin-bottom: 20px; /* Agrega más margen inferior en pantallas grandes */
+  flex: 1; /* El texto ocupará todo el ancho disponible en pantallas más pequeñas */
+
+  @media (min-width: 1024px) {
+    font-size: 28px; /* Aumenta el tamaño de la fuente en pantallas más grandes */
+  }
+
+  @media (max-width: 1024px) {
+    text-align: center;
+    font-size: 20px;
+    margin-right: 0; /* Elimina el espacio entre el texto y la imagen en pantallas más pequeñas */
+  }
+`;
+
+const ImgPart = styled.img`
+  object-fit: cover;
+  max-width: 50%; /* La imagen ocupará el 50% del ancho disponible en pantallas grandes */
+  height: auto;
+
+  @media (min-width: 1024px) {
+    max-height: 600px; /* Aumenta la altura máxima de la imagen en pantallas más grandes */
+    width: 100%; /* Hace que la imagen ocupe todo el ancho disponible */
+  }
+
+  @media (max-width: 425px) {
+    min-height: 350px;
+  }
+`;
