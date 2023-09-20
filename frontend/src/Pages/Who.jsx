18c27e8727f7a7402157d4e057c501c8265f54bd
JPGenaro <jpgenaro06@gmail.com>
2023-09-20 10:54:45 -0300

    al fin, todo responsive, falta los integrantes nomas

--- frontend/src/Pages/Who.jsx
+++ frontend/src/Pages/Who.jsx
@@ -6,56 +6,73 @@ import Button from '@mui/material/Button';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import CardTeam from '../components/Who/CardTeam'; // Cambia el nombre del componente
+import CardTeam from '../components/Who/CardTeam';
 import NavBar from '../components/NavBar/Navbar';
 
 function Who() {
   return (
     <>
       <NavBar />
+      
       <Zoom top>
         <Wrap_2>
-          <Typography variant="h3">¿QUIÉNES SOMOS?</Typography>
+          <TitleContainer>
+            <StyledTitle variant="h3">¿QUIÉNES SOMOS?</StyledTitle>
+          </TitleContainer>
         </Wrap_2>
       </Zoom>
+
       <Wrap>
         <Fade bottom>
-          <Container>
-            <Typography variant="body1">
-              Somos un grupo comprometido de estudiantes y amantes de los animales del Instituto
-              Tecnico Salesiano Villada. Nuestro propósito es brindarles una oportunidad de encontrar un
-              hogar responsable. Creemos firmemente que cada perro merece una segunda oportunidad y el
-              cariño incondicional de una familia. Por ello, hemos creado esta plataforma para conectar a
-              perros rescatados con personas que estén dispuestas a darles un hogar lleno de amor.
-            </Typography>
-          </Container>
-          <ImageContainer>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Container>
+                <StyledText variant="body1">
+                  Somos un grupo comprometido de estudiantes y amantes de los animales del Instituto
+                  Tecnico Salesiano Villada. Nuestro propósito es brindarles una oportunidad de encontrar un
+                  hogar responsable. Creemos firmemente que cada perro merece una segunda oportunidad y el
+                  cariño incondicional de una familia. Por ello, hemos creado esta plataforma para conectar a
+                  perros rescatados con personas que estén dispuestas a darles un hogar lleno de amor.
+                </StyledText>
+              </Container>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <ImageContainer>
+                <CardMedia
+                  component="img"
+                  alt="Imagen de muestra"
+                  height="auto"
+                  image="https://images.ecestaticos.com/jLESCdMEXIc9qGG2FOpA1NP4H1w=/0x0:2121x1414/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fba6%2F896%2Ff44%2Fba6896f446572c38b60115e358ae8ccb.jpg"
+                />
+              </ImageContainer>
+            </Grid>
+          </Grid>
+        </Fade>
+      </Wrap>
+      
+      <Wrap>
+        <StyledTitle variant="h4">Un poco de nuestra historia</StyledTitle>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
             <CardMedia
               component="img"
               alt="Imagen de muestra"
-              height="600"
-              image="https://images.ecestaticos.com/jLESCdMEXIc9qGG2FOpA1NP4H1w=/0x0:2121x1414/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fba6%2F896%2Ff44%2Fba6896f446572c38b60115e358ae8ccb.jpg"
+              height="auto"
+              image="https://cdn.wikiwiki.jp/to/w/becomnextbot/Wenomechainsama/::attach/Wenomechainsama.png?rev=08e3f8c3e8252bc599d086a85d402b2d&t=20221226165443"
             />
-          </ImageContainer>
-        </Fade>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Container>
+              <StyledText variant="body1">
+                Todo comenzó a principios de este año, donde teníamos que realizar una página sin fines de lucro. Después de una lluvia de ideas, nos decidimos por hacer un proyecto que ayude y facilite la adopción de perros. Así nació: Dame La Pata
+              </StyledText>
+              <StyledText variant="body1">
+                La idea surgió al ver la triste realidad de muchos perros abandonados y maltratados en nuestra comunidad. Queríamos marcar una diferencia y contribuir a mejorar sus vidas. Así que nos pusimos manos a la obra y creamos "Dame La Pata", un espacio en línea donde los refugios y protectoras de animales pueden publicar perfiles de los perros disponibles para adopción, y las personas interesadas en adoptar pueden buscar y filtrar perros según sus preferencias y ubicación.
+              </StyledText>
+            </Container>
+          </Grid>
+        </Grid>
       </Wrap>
-      {/* Contenido del componente History integrado */}
-      <Container>
-        <Typography variant="h4">Un poco de nuestra historia</Typography>
-        <CardMedia
-          component="img"
-          alt="Imagen de muestra"
-          height="400"
-          image="https://cdn.wikiwiki.jp/to/w/becomnextbot/Wenomechainsama/::attach/Wenomechainsama.png?rev=08e3f8c3e8252bc599d086a85d402b2d&t=20221226165443"
-        />
-        <Typography variant="body1">
-          Todo comenzó a principios de este año, donde teníamos que realizar una página sin fines de lucro. Después de una lluvia de ideas, nos decidimos por hacer un proyecto que ayude y facilite la adopción de perros. Así nació: Dame La Pata
-        </Typography>
-        <Typography variant="body1">
-          La idea surgió al ver la triste realidad de muchos perros abandonados y maltratados en nuestra comunidad. Queríamos marcar una diferencia y contribuir a mejorar sus vidas. Así que nos pusimos manos a la obra y creamos "Dame La Pata", un espacio en línea donde los refugios y protectoras de animales pueden publicar perfiles de los perros disponibles para adopción, y las personas interesadas en adoptar pueden buscar y filtrar perros según sus preferencias y ubicación.
-        </Typography>
-      </Container>
-      {/* Fin del contenido del componente History integrado */}
       <CardTeam />
     </>
   );
@@ -63,6 +80,7 @@ function Who() {
 
 export default Who;
 
+// Estilos
 const Wrap = styled.div`
   width: 100%;
   display: flex;
@@ -70,14 +88,21 @@ const Wrap = styled.div`
   align-items: center;
 `;
 
+const StyledTitle = styled(Typography)`
+  /* Anular el margin predeterminado de Material UI */
+  margin: 20px 0 !important; /* Agregar margen arriba y abajo */
+  font-size: 54px !important; /* Ajusta el tamaño de la fuente para títulos */
+  font-weight: bold !important; /* Hacer que los títulos estén en negrita */
+  text-align: center; /* Centrar el texto */
+`;
+
+const StyledText = styled(Typography)`
+  font-size: 24px !important; /* Ajusta el tamaño de la fuente para el texto */
+`;
+
 const Container = styled.div`
   width: 100%;
   padding: 20px;
-
-  @media (min-width: 1024px) {
-    width: 50%;
-    padding-right: 20px;
-  }
 `;
 
 const ImageContainer = styled.div`
@@ -93,4 +118,8 @@ const Wrap_2 = styled.div`
   @media (max-width: 425px) {
     height: auto;
   }
+`;
+
+const TitleContainer = styled.div`
+  margin-top: 60px; /* Agregar margen superior en pantallas pequeñas */
 `;
