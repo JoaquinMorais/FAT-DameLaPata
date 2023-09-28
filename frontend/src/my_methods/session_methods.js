6288ced9a824d21fdb2d5ae6d504e4211f6ef88a
Agus Salonia <agusalonia04@gmail.com>
2023-09-28 11:48:15 -0300

    navbar arreglado (90%)

--- frontend/src/my_methods/session_methods.js
+++ frontend/src/my_methods/session_methods.js
@@ -7,12 +7,12 @@ export async function FetchNavbarItems() {
     try{
       if(localStorage.getItem('id') !== null){
         if (localStorage.getItem('type') === 'adopter'){
-          pages_array = ['Inicio', 'Quienes Somos', 'Adoptar']
-          setting_array = ['Mi Perfil']//'Cerrar Session'
+          pages_array = ['Inicio', 'Quienes Somos', 'Adoptar', 'Peticiones']
+          setting_array = ['Mi Perfil', 'Cerrar Sesion'] //Te manda a login, no cierra sesion
         }
         else if (localStorage.getItem('type') === 'shelter'){
-          pages_array = ['Inicio', 'Quienes Somos', 'Publicar']
-          setting_array = ['Perfil del Refugio'] 
+          pages_array = ['Inicio', 'Quienes Somos', 'Publicar', 'Mis Mascotas']
+          setting_array = ['Mi Refugio', 'Cerrar Sesion'] //Te manda a login, no cierra sesion
         }
       }   
       else{
