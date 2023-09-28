dae725715df17377565b4457dc8ccb4ae4290cf4
Marco <marco.fini22@gmail.com>
2023-09-28 20:57:06 -0300

    pagina error por ahora

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -1,25 +1,77 @@
 import React from 'react';
 import Navbar from '../components/NavBar/NavBar';
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import { purple } from '@mui/material/colors';
+import { styled } from '@mui/material';
 
 function Error() {
+  const ColorButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: 'green', 
+    borderColor: 'green', 
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: 'lightgreen', 
+      borderColor: 'darkgreen', 
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: 'darkgreen', 
+      borderColor: 'green', 
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  });
+  
   return (
     <>
       <Navbar />
       <div style={styles.centeredContent}>
-        <h1 style={{ fontSize: '100px', fontWeight: 'bold' }}>TREMENDO ERROR AMIGO JAJAJAJA</h1>
+        <h1 style={{ fontSize: '5vw', fontWeight: 'bold', textAlign: 'center' }}>Lo sentimos, ha ocurrido un error :c</h1>
+        <img src="https://img.freepik.com/fotos-premium/terrier-brasileno-sobre-fondo-blanco_87557-14811.jpg" alt="" style={styles.image} />
+        <ColorButton variant="contained" startIcon={<HomeIcon />} style={styles.button}>
+          <a href="/" style={{ color: 'white', fontWeight: 'bold', textDecoration: 'none' }}>VOLVER AL INICIO</a>
+        </ColorButton>
       </div>
     </>
   );
 }
 
 const styles = {
-    centeredContent: {
+  centeredContent: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    height: '100vh', 
+    minHeight: '100vh',
+    padding: '20px',
+    textAlign: 'center',
+  },
+  image: {
+    maxWidth: '100%',
+    height: 'auto',
+    marginTop: '20px',
   },
 };
+
 
 export default Error;
