090767fac464c7e99b7cb5b791cd97085270ed8b
JPGenaro <jpgenaro06@gmail.com>
2023-09-28 11:30:34 -0300

    optimizacion y redireccion de errores

--- frontend/src/my_methods/query_methods.js
+++ frontend/src/my_methods/query_methods.js
@@ -1,45 +1,39 @@
 import axios from "axios";
-import { Navigate } from "react-router";
-
-
-
-let colors_array = ['']
-let sizes_array = ['']
+import { useNavigate } from "react-router-dom";
 
 async function GetPreference() {
-    try {
-      const response_color = await axios.get('http://localhost:5000/adopter/tastes/colors', '');
-      const response_size = await axios.get('http://localhost:5000/adopter/tastes/sizes', '');
+  const navigate = useNavigate();
 
-      if (response_color.data['status'] === 402){
-        console.log(response_color);
-        <Navigate path='/error'/>
-      }
-      else if(response_color.data['status'] === 200){
-        colors_array = response_color.data.colors;
-        response_color = console.log(response_color);
-        console.log(response_color);
+  try {
+    const response_color = await axios.get('http://localhost:5000/adopter/tastes/colors', '');
+    const response_size = await axios.get('http://localhost:5000/adopter/tastes/sizes', '');
 
-      }
-      if (response_size.data['status'] === 402){
-        console.log(response_size);
-        <Navigate path='/error'/>
-      }
-      else if(response_size.data['status'] === 200){
-        sizes_array = response_size.data.sizes;
-        response_size = console.log(response_size);
-        console.log(response_size);
-      }
-    } 
-    
-    catch (error) {
-      console.error('Error al realizar la solicitud:', error.message);
+    if (response_color.data.status === 402 || response_size.data.status === 402) {
+      // Manejar el error 402 y redirigir a la página de error
+      navigate('/error');
+      return { colors_array: [], sizes_array: [] };
     }
 
-    return{
-      colors_array: colors_array,
-      sizes_array: sizes_array
+    if (response_color.data.status === 200) {
+      const colors_array = response_color.data.colors;
+      console.log('Colores:', colors_array);
     }
+
+    if (response_size.data.status === 200) {
+      const sizes_array = response_size.data.sizes;
+      console.log('Tamaños:', sizes_array);
+    }
+
+    return {
+      colors_array: colors_array || [],
+      sizes_array: sizes_array || [],
+    };
+  } catch (error) {
+    console.error('Error al realizar la solicitud:', error.message);
+    // Manejar otros errores si es necesario
+    navigate('/error');
+    return { colors_array: [], sizes_array: [] };
+  }
 }
 
-export default GetPreference
+export default GetPreference;
