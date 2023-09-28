37c4217982393ab60eb3679a7335055351fac808
Marco <marco.fini22@gmail.com>
2023-09-28 20:40:09 -0300

    caca

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Navbar from '../components/NavBar/NavBar';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 function Error() {
   return (
     <>
       <Navbar />
       <div style={styles.centeredContent}>
-        <h1 style={{ fontSize: '100px', fontWeight: 'bold' }}>TREMENDO ERROR AMIGO JAJAJAJA</h1>
+        <h1 style={{ fontSize: '50px', fontWeight: 'bold' }}>Lo sentimos, ha ocurrido un error :c</h1>
+        <img src="https://img.freepik.com/fotos-premium/terrier-brasileno-sobre-fondo-blanco_87557-14811.jpg" alt="" />
+        <Button variant="outlined" startIcon={<DeleteIcon />}>
+        <a href="/">VOLVER AL INICIO</a>
+        </Button>
       </div>
     </>
   );
