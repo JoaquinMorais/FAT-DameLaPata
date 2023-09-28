e64208513b24139047c083b614ce79803a0e4d99
JPGenaro <jpgenaro06@gmail.com>
2023-09-28 11:51:23 -0300

    finish (mejoras hechas)

--- frontend/src/my_methods/dogs_methods.js
+++ frontend/src/my_methods/dogs_methods.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
-var response = {}
+/**
+ * Envía una solicitud PUT para agregar un perro con los datos proporcionados.
+ * @param {object} values - Los datos del perro a agregar.
+ * @returns {Promise<object>} - Una promesa que resuelve en la respuesta del servidor.
+ * Si hay un error, se rechaza con un objeto que contiene un mensaje de error.
+ */
 
 export async function PutDogs(values) {
-    try {
-        response = await axios.put('http://localhost:5000/pet', values);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-      return response
-  };
+  try {
+    const response = await axios.put('http://localhost:5000/pet', values);
+    return response.data; // Devuelve los datos de la respuesta
+  } catch (error) {
+    console.error('Error en PutDogs:', error);
+    // Manejo de error más detallado
+    throw new Error('No se pudo agregar el perro. Por favor, inténtalo de nuevo.');
+  }
+}
