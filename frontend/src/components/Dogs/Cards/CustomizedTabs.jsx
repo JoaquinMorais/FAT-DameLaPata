29b5fc600b9fb8995c0bf5a49158c493c39833fc
Yaco Babiachuk <babiachukyaco@gmail.com>
2023-10-19 11:38:27 -0300

    tabs agregados

---
+++ frontend/src/components/Dogs/Cards/CustomizedTabs.jsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import ShelterContent from './ShelterContent';
+import PetContent from './PetContent';
+
+const StyledTabs = styled((props) => (
+  <Tabs
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+  />
+))({
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  '& .MuiTabs-indicatorSpan': {
+    maxWidth: 40,
+    width: '100%',
+    backgroundColor: '#000',
+  },
+});
+
+const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: 'none',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    color: 'rgba(255, 255, 255, 0.7)',
+    '&.Mui-selected': {
+      color: '#000',
+    },
+    '&.Mui-focusVisible': {
+      backgroundColor: 'rgba(100, 95, 228, 0.32)',
+    },
+  }),
+);
+
+export default function CustomizedTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ bgcolor: '#ffae8d' }}>
+        <StyledTabs
+          value={value}
+          onChange={handleChange}
+          aria-label="styled tabs example"
+        >
+          <StyledTab label="MASCOTA" />
+          <StyledTab label="REFUGIO" />
+        </StyledTabs>
+        <Box sx={{ p: 3 }} />
+      </Box>
+      <Box sx={{ p: 3 }}>
+        {value === 0 ? <PetContent /> : <ShelterContent />}
+      </Box>
+    </Box>
+  );
+};
